8098383d0307fe61bfb2f9e10e09aefccdc9bfd0
ShuitiKawahara <kawaharasyuichi@gmail.com>
2019-10-31 17:07:14 +0900

    Corresponding to leap years

--- 01-IMAP/get_spammail.py
+++ 01-IMAP/get_spammail.py
@@ -1,10 +1,7 @@
-# This program collects spam mail from the mail server (IMAP).
-# This program is running on Raspberry Pi. Every day at midnight, spam emails from the previous day are collected.
-
 import imaplib
 import email
 import datetime
-import something_info
+import something_info  # メールサーバのパスワード等の情報を記載したファイル
 
 from email.header import make_header, decode_header
 
@@ -12,23 +9,52 @@ docomo = imaplib.IMAP4_SSL(something_info.SERVER_NAME, 993)
 docomo.login(something_info.USER_ID, something_info.PASSWORD)
 docomo.select('inbox')
 
-month_dict = {1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun",
-              7: "Jul", 8: "Aug", 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec"}
-
-date = datetime.date.today()
-today = str(date.day) + '-' + month_dict[date.month] + '-' + str(date.year)
-yesterday = str(date.day - 1) + '-' + \
-    month_dict[date.month] + '-' + str(date.year)
-
-search_option = 'SINCE ' + yesterday + ' BEFORE ' + today
-_, datas = docomo.search(None, search_option)
+month_dict = {1: ["Jan", 31], 2: ["Feb", 28, 29], 3: ["Mar", 31], 4: ["Apr", 30], 5: ["May", 31], 6: ["Jun", 30],
+              7: ["Jul", 31], 8: ["Aug", 31], 9: ["Sep", 30], 10: ["Oct", 31], 11: ["Nov", 30], 12: ["Dec", 31]}
 
 
-def write_mailtext(msg, skip_mail_num):
+def isLeapYear(year):
+    return (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0))
+
+
+def make_yesterday_str():
+    date = datetime.date.today()
+    today = str(date.day) + '-' + \
+        month_dict[date.month][0] + '-' + str(date.year)
+
+    if (date.today - 1) == 0:  # 月初
+        if (date.month == 3 and isLeapYear(data.year)):  # 前日が2月かつ閏年の場合
+            yesterday = str(month_dict[2][2]) + '-' + \
+                month_dict[date.month - 1][0] + '-' + str(date.year)
+
+        elif (date.month == 3):  # 前日が2月かつ閏年でない場合
+            yesterday = str(month_dict[2][1]) + '-' + \
+                month_dict[date.month - 1][0] + '-' + str(date.year)
+
+        elif (date.month == 1):  # 前日が12月の場合
+            yesterday = str(month_dict[12][1]) + '-' + \
+                month_dict[12][0] + '-' + str(date.year - 1)
+
+        else:
+            yesterday = str(month_dict[date.month-1][1]) + '-' + \
+                month_dict[date.month-1][0] + '-' + str(date.year)
+
+    else:
+        yesterday = str(date.day - 1) + '-' + \
+            month_dict[date.month] + '-' + str(date.year)
+
+    search_option = 'SINCE ' + yesterday + ' BEFORE ' + today
+
+    return yesterday, search_option
+
+
+def write_mailtext(msg, yesterday, skip_mail_num):
+
     try:
         from_addr = str(make_header(decode_header(msg["From"])))
     except:
         skip_mail_num += 1
+        return
 
     if from_addr in something_info.notspam_address_list:  # スパムではないアドレスの確認
         # スパムでないアドレスが確認できた場合の処理
@@ -81,6 +107,7 @@ def write_mailtext(msg, skip_mail_num):
 
             if charset is not None:
                 payload = payload.decode(charset, "ignore")
+
             try:
                 payload = payload.replace('\r\n', '')
                 payload = payload.replace('\n', '')
@@ -100,26 +127,33 @@ def write_mailtext(msg, skip_mail_num):
                 else:
                     with open(something_info.spam_path + "spam_" + yesterday + ".txt", "a") as spam:
                         spam.write("Decode_Error")
+
         with open(something_info.spam_path + "spam_" + yesterday + ".txt", "a") as spam:
             spam.write("\n"*2)
 
 
 # 取得したメール一覧の処理
 skip_mail_num = 0
+yesterday, search_option = make_yesterday_str()
 
-for num in datas[0].split():
+_, datas = docomo.search(None, search_option)
+
+for i, num in enumerate(datas[0].split()):
     _, data = docomo.fetch(num, '(RFC822)')
 
     try:
         msg = email.message_from_bytes(data[0][1])
     except:
         skip_mail_num += 1
+        continue
 
-    write_mailtext(msg, skip_mail_num)
+    write_mailtext(msg, yesterday, skip_mail_num)
+
+    print(i+1)
 
 with open(something_info.spam_path + "spam_" + yesterday + ".txt", "a") as spam:
     spam.write("Total spam mail num:" +
-               str(len(datas[0].split())-skip_mail_num)+"\n")
+               str(len(datas[0].split())-skip_mail_num))
 
 docomo.close()
 docomo.logout()
