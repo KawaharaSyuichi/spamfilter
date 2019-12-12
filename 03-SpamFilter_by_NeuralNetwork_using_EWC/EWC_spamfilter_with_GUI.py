66260fc969d259a7732ddb0c2dc29a6c0584a071
ShuitiKawahara <kawaharasyuichi@gmail.com>
2019-12-12 16:03:19 +0900

    add Train_and_Test class

--- 03-SpamFilter_by_NeuralNetwork_using_EWC/EWC_spamfilter_with_GUI.py
+++ 03-SpamFilter_by_NeuralNetwork_using_EWC/EWC_spamfilter_with_GUI.py
@@ -20,6 +20,83 @@ def cal_time(func):  # 実行時間の計測
     return wrapper
 
 
+class Train_and_Test:
+    def __init__(self, doc2vec_info_list, nn_model_info):
+        self.nn_model_info = nn_model_info
+        self.doc2vec_info_list = doc2vec_info_list
+        self.lams = [0]
+        self.num_iter = 320
+        self.batch_size = 40
+        self.disp_freq = 20
+        self.test_accs = np.zeros(int(self.num_iter/self.disp_freq))
+
+    def plot_test_acc(self, plot_handles):
+        plt.legend(handles=plot_handles, loc="lower right")
+        plt.xlabel("Iterations")
+        plt.ylabel("Test Accuracy")
+        plt.yticks(np.arange(0.0, 1.1, 0.1))
+        plt.xticks(np.arange(0, 310, 50))
+        plt.ylim(0.0, 1.01)
+        plt.grid(which='major', color='black', linestyle='-')
+        display.display(plt.gcf())
+        display.clear_output(wait=True)
+
+    def set_test_accs(self):
+        self.test_accs = np.zeros(int(self.num_iter/self.disp_freq))
+
+    def random_batch(self, mail_vector):
+        train_batch = list()
+        vector_batch = list()
+        type_batch = list()
+
+        vector_batch.extend(mail_vector.spam_vector[:self.batch_size])
+        vector_batch.extend(mail_vector.ham_vector[:self.batch_size])
+
+        train_batch.append(vector_batch)
+
+        type_batch.extend([[0.0, 1.0] for _ in range(self.batch_size)])  # spam
+
+        type_batch.extend([[1.0, 0.0] for _ in range(self.batch_size)])  # ham
+
+        return train_batch
+
+    def test(self, iter):
+        test_batch = self.random_batch(self.doc2vec_info_list[0])
+
+        test_feed_dict = {self.nn_model_info.mail_doc2vec: np.array(
+            test_batch[0]), self.nn_model_info.mail_class: np.array(test_batch[1])}
+
+        self.test_accs[int(iter/self.disp_freq)
+                       ] = self.nn_model_info.nn_model.accuracy.eval(feed_dict=test_feed_dict)
+
+        plt.plot(range(1, iter + 2, self.disp_freq), self.test_accs)
+        plt.show()
+
+    def train(self):
+        for l in range(len(self.lams)):
+            self.nn_model_info.nn_model.restore(self.nn_model_info.sess)
+
+            if (self.lams[l] == 0):
+                self.nn_model_info.nn_model.set_vanilla_loss()
+            else:
+                self.nn_model_info.nn_model.update_ewc_loss(self.lams[l])
+
+            self.set_test_accs()
+
+            for iter in range(self.num_iter):
+                train_batch = self.random_batch(
+                    self.doc2vec_info_list[0])
+
+                train_feed_dict = {self.nn_model_info.mail_doc2vec: np.array(
+                    train_batch[0]), self.nn_model_info.mail_class: np.array(train_batch[1])}
+
+                self.nn_model_info.nn_model.train_step.run(
+                    feed_dict=train_feed_dict)
+
+                if iter % self.disp_freq == 0:
+                    self.test(iter)
+
+
 class Doc2vecInformation:
     def __init__(self):
         self.file_path = str()
@@ -39,12 +116,18 @@ class Doc2vecInformation:
     def set_mail_type(self, mail_type):
         self.mail_type = mail_type
 
+    def set_border(self, border):
+        self.border = border
+
+    def get_vector_size(self):
+        return len(self.spam_vector)
+
 
 class NeuralNetworkInformation:
-    def __init__(self, mail_doc2vec_vector_size):  # current mail_doc2vec_vector_size : 300
+    def __init__(self, VECTOR_SIZE):  # current mail_doc2vec_vector_size : 300
         self.sess = tf.InteractiveSession()
         self.mail_doc2vec = tf.placeholder(
-            tf.float32, shape=[None, mail_doc2vec_vector_size])
+            tf.float32, shape=[None, VECTOR_SIZE])
         self.mail_class = tf.placeholder(
             tf.float32, shape=[None, 2])  # ham:[1,0] spam:[0,1]
         self.nn_model = Model(self.mail_doc2vec, self.mail_class)
@@ -53,7 +136,7 @@ class NeuralNetworkInformation:
         self.sess.run(tf.global_variables_initializer())
 
 
-def read_doc2vec():
+def read_doc2vec(doc2vec_info_list):
     while True:
         doc2vec_info = Doc2vecInformation()
         doc2vec_info.set_file_path(doc2vec_model_select.read_doc2vec_model())
@@ -64,7 +147,7 @@ def read_doc2vec():
 
         # doc2vecのどこまでがspamで，どこからがhamメールかを入力
         doc2vec_border = input('Input doc2vec border >>')
-        doc2vec_info.border = int(doc2vec_border)
+        doc2vec_info.set_border(int(doc2vec_border))
 
         doc2vec_info.separata_spam_and_ham(
             Doc2Vec.load(doc2vec_info.file_path))
@@ -78,4 +161,10 @@ def read_doc2vec():
 
 
 doc2vec_info_list = []
-read_doc2vec()
+read_doc2vec(doc2vec_info_list)
+
+nn_model_info = NeuralNetworkInformation(
+    doc2vec_info_list[0].get_vector_size())
+nn_model_info.sess_run()
+
+Train_and_Test(doc2vec_info_list, nn_model_info).train()
