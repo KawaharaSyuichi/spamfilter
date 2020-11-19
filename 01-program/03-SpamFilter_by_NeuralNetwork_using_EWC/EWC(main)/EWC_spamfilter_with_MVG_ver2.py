50df44aec081a043cbe84b5d8f8522375301e807
ShuitiKawahara <kawaharasyuichi@gmail.com>
2020-11-19 19:19:57 +0900

    add MVG program ver2

---
+++ 01-program/03-SpamFilter_by_NeuralNetwork_using_EWC/EWC(main)/EWC_spamfilter_with_MVG_ver2.py
@@ -0,0 +1,448 @@
+"""
+2020/11/24日の輪行用のプログラム(詳しいことはOneDriveのPPTを参照)
+"""
+import sys
+import csv
+import copy
+import gensim
+import random
+import warnings
+import matplotlib
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from IPython import display
+from gensim.models import Doc2Vec
+from collections import OrderedDict
+from model_for_doc2vec_ver2 import Model
+
+random.seed(2020)
+tf.compat.v1.set_random_seed(2020)
+warnings.filterwarnings(
+    'ignore', category=matplotlib.MatplotlibDeprecationWarning)
+
+
+class parameter:
+    """
+    ニューラルネットワークで使用するパラメータ
+    """
+
+    def __init__(self):
+        self.ITERATIONS = 1220  # 元は520
+        self.DISP_FREQ = 20  # 元は20
+        self.lams = [0]
+
+    def set_lams(self, lams_value):
+        self.lams.append(lams_value)
+
+
+def random_batch(trainset, batch_size, start_idx):
+    """
+    学習用のバッチ作成
+    """
+
+    half_batch_size = int(batch_size / 2)
+    return_flag = False
+
+    batch = []
+    docvec = []
+    docflag = []
+    idx = []
+
+    spam_half_start = 100 + start_idx * half_batch_size
+    spam_half_end = 100 + (start_idx + 1) * half_batch_size
+    ham_half_start = 1100 + start_idx * half_batch_size
+    ham_half_end = 1100 + (start_idx + 1) * half_batch_size
+
+    if spam_half_end > 500:  # 学習用データセットを一通り学習した場合(エポック数が1増える)
+        idx = [num for num in range(spam_half_start, 500)]
+        idx.extend(random.sample(range(100, spam_half_start),
+                                 k=half_batch_size - len(idx)))
+        idx_1 = [num for num in range(ham_half_start, 1500)]
+        idx_1.extend(random.sample(range(1100, ham_half_start),
+                                   k=half_batch_size - len(idx_1)))
+
+        # 学習用データセットをシャッフル
+        spam_half = trainset[100:500]
+        ham_half = trainset[1100:1500]
+        random.shuffle(spam_half)
+        random.shuffle(ham_half)
+        trainset[100:500] = spam_half
+        trainset[1100:1500] = ham_half
+
+        return_flag = True
+    else:
+        idx = [num for num in range(spam_half_start, spam_half_end)]
+        random.shuffle(idx)
+        idx_1 = [num for num in range(ham_half_start, ham_half_end)]
+        random.shuffle(idx_1)
+    idx.extend(idx_1)
+
+    for num in idx:
+        docvec.append(trainset[num])
+
+        # ラベル作成
+        if num < 1000:
+            docflag.append([0.0, 1.0])  # spam
+        else:
+            docflag.append([1.0, 0.0])  # ham
+
+    batch.append(docvec)
+    batch.append(docflag)
+
+    return batch, return_flag
+
+
+def make_test_batch(trainset):
+    """
+    テスト用のバッチ作成
+    """
+
+    batch = []
+    docvec = []
+    docflag = []
+
+    # テスト用バッチでは学習用で用いなかった残りのデータを全て使用する
+    idx = [num for num in range(500, 1000)]  # スパムメール分
+    idx_1 = [num for num in range(1500, 2000)]  # 正規メール分
+    idx.extend(idx_1)
+
+    for num in idx:
+        docvec.append(trainset[num])
+
+        if num < 1000:
+            docflag.append([0.0, 1.0])  # spam
+        else:
+            docflag.append([1.0, 0.0])  # ham
+
+    batch.append(docvec)
+    batch.append(docflag)
+
+    return batch
+
+
+def plot_test_acc(plot_handles):
+    """
+    学習結果のプロット図を作成
+    """
+
+    plt.legend(handles=plot_handles, loc="lower right")
+    plt.xlabel("Iterations")
+    plt.ylabel("Test Accuracy")
+    plt.yticks(np.arange(0.45, 1.1, 0.05))
+    plt.xticks(np.arange(0, 520, 20))  # 元は0,520,20
+    plt.ylim(0.45, 1.01)
+    plt.grid(which='major', color='black', linestyle='-')
+    display.display(plt.gcf())
+    display.clear_output(wait=True)
+
+
+def train_task(model, num_iter, disp_freq, trainset, doc2vec_labels, testsets, mail_doc2vec, mail_class, sess, lams):
+    """
+    学習と識別の実行
+    """
+
+    train_start_idx = 0
+    train_ephoc = 0
+    mvg_train_start_idx = 0
+    mvg_train_ephoc = 0
+
+    for l in range(len(lams)):
+        first_flag = False
+
+        model.restore(sess)  # 重みとバイアスのパラメータの読み込み
+
+        if(lams[l] == 0):
+            model.set_vanilla_loss()  # 確率的勾配降下法を用いる
+        else:
+            model.update_ewc_loss(lams[l])  # Elastic Weight Consolidationを用いる
+
+        # 識別率を格納するためのリスト作成
+        test_accs = []
+        for task in range(len(testsets)):
+            test_accs.append(np.zeros(int(num_iter / disp_freq)))
+
+        # num_iter回学習を行う
+        for iter in range(num_iter):
+            # 初学習の場合と学習回数が20回以上の場合は新しい学習データでのみ学習
+            if iter > 100 or len(testsets) == 1:
+                if len(lams) == 2 and first_flag == True:  # 初の学習を行う場合
+                    train_batch, return_flag = random_batch(
+                        trainset, 32, train_start_idx)
+
+                    if return_flag == False:
+                        train_start_idx += 1
+                    elif return_flag == True:
+                        train_start_idx = 0
+                        train_ephoc += 1
+                        print("train ephoc is {}".format(train_ephoc))
+
+                    # 学習開始
+                    model.train_step.run(feed_dict={mail_doc2vec: np.array(
+                        train_batch[0]), mail_class: np.array(train_batch[1])})
+
+                elif len(lams) == 2 and first_flag == False:
+                    first_flag = True
+                elif len(lams) == 1:
+                    train_batch, return_flag = random_batch(
+                        trainset, 32, train_start_idx)
+
+                    if return_flag == False:
+                        train_start_idx += 1
+                    elif return_flag == True:
+                        train_start_idx = 0
+                        train_ephoc += 1
+                        print("train ephoc is {}".format(train_ephoc))
+
+                    # 学習開始
+                    model.train_step.run(feed_dict={mail_doc2vec: np.array(
+                        train_batch[0]), mail_class: np.array(train_batch[1])})
+
+            elif iter % 2 == 0:  # 学習回数が100回未満、かつ学習回数が偶数回の場合
+                if len(lams) == 2 and first_flag == True:  # 初の学習を行う場合
+                    train_batch, return_flag = random_batch(
+                        trainset, 32, train_start_idx)
+
+                    if return_flag == False:
+                        train_start_idx += 1
+                    elif return_flag == True:
+                        train_start_idx = 0
+                        train_ephoc += 1
+                        print("train ephoc is {}".format(train_ephoc))
+
+                    # 学習開始
+                    model.train_step.run(feed_dict={mail_doc2vec: np.array(
+                        train_batch[0]), mail_class: np.array(train_batch[1])})
+
+                elif len(lams) == 2 and first_flag == False:
+                    first_flag = True
+                elif len(lams) == 1:
+                    train_batch, return_flag = random_batch(
+                        trainset, 32, train_start_idx)
+
+                    if return_flag == False:
+                        train_start_idx += 1
+                    elif return_flag == True:
+                        train_start_idx = 0
+                        train_ephoc += 1
+                        print("train ephoc is {}".format(train_ephoc))
+
+                    # 学習開始
+                    model.train_step.run(feed_dict={mail_doc2vec: np.array(
+                        train_batch[0]), mail_class: np.array(train_batch[1])})
+
+            else:  # 学習回数が100回未満、かつ学習回数が奇数回の場合(つまりMVG)
+                mvg_train_batch, return_flag = random_batch(
+                    testsets[len(testsets)-1], 32, mvg_train_start_idx)
+
+                if return_flag == False:
+                    mvg_train_start_idx += 1
+                elif return_flag == True:
+                    mvg_train_start_idx = 0
+                    mvg_train_ephoc += 1
+                    print("mvg train ephoc is {}".format(mvg_train_ephoc))
+
+                # 学習開始
+                model.train_step.run(feed_dict={mail_doc2vec: np.array(
+                    mvg_train_batch[0]), mail_class: np.array(mvg_train_batch[1])})
+
+            # disp_freq(=20)回学習するごとに、テスト用メールに対する識別率を求める
+            if iter % disp_freq == 0:
+                plt.subplot(1, len(lams), l + 1)
+
+                plots = []
+
+                for task in range(len(testsets)):
+                    test_batch = make_test_batch(testsets[task])
+
+                    feed_dict = {mail_doc2vec: np.array(
+                        test_batch[0]), mail_class: np.array(test_batch[1])}
+
+                    # テスト用データに対する識別率を算出
+                    test_accs[task][int(
+                        iter / disp_freq)] = model.accuracy.eval(feed_dict=feed_dict)
+
+                    usage_guide = doc2vec_labels[task]
+
+                    # 1回学習するごとに識別率を表示
+                    if l == 0:
+                        print("SGD" + " " + usage_guide + ":" +
+                              str(test_accs[task][int(iter / disp_freq)]))
+                    else:
+                        print("EWC" + " " + usage_guide + ":" +
+                              str(test_accs[task][int(iter / disp_freq)]))
+
+                    plot_h, = plt.plot(range(
+                        1, iter + 2, disp_freq), test_accs[task][:int(iter / disp_freq) + 1], marker='.', label=usage_guide)
+
+                    plots.append(plot_h)
+
+                plot_test_acc(plots)
+
+                if l == 0:
+                    plt.title("Stochastic Gradient Descent")
+                else:
+                    plt.title("Elastic Weight Consolidation")
+
+                plt.gcf().set_size_inches(len(lams) * 5, 3.5)
+
+    plt.show()
+
+
+def mvg(model, mail_doc2vec, mail_class, doc2vec_dict, num_iter, disp_freq, sess, lams):
+    train_start_idx = 0
+    doc2vec_2005 = copy.copy(doc2vec_dict['2005'])
+    doc2vec_2006 = copy.copy(doc2vec_dict['2006'])
+    doc2vec_2007 = copy.copy(doc2vec_dict['2007'])
+
+    for l in range(len(lams)):
+        # パラメータ読み込み
+        model.restore(sess)
+
+        if (lams[l] == 0):
+            # only SDG
+            model.set_vanilla_loss()
+        else:
+            # EWC
+            model.update_ewc_loss(lams[l])
+
+        # 識別率を格納するためのリストを作成
+        test_accs = [[], [], []]  # 順に2005,2006,2007(train_accsも同様)
+        train_accs = [[], [], []]
+
+        # どの年から学習するかの設定
+        switch_flag = 2005
+
+        for iteration in range(num_iter):
+            if iteration < 600:  # # 2005年と2006年を20iterationごとに切り替えて学習
+                if iteration == 0:  # 最初は2005年を学習
+                    train_doc2vec = copy.copy(
+                        doc2vec_dict['2005'])
+                elif iteration % 20 == 0 and switch_flag == 2005:  # 2006年の学習に切り替え
+                    train_doc2vec = copy.copy(
+                        doc2vec_dict['2006'])
+                    switch_flag = 2006
+                elif iteration % 20 == 0 and switch_flag == 2006:  # 2005年の学習に切り替え
+                    train_doc2vec = copy.copy(
+                        doc2vec_dict['2005'])
+                    switch_flag = 2005
+            elif iteration == 600:  # 2007年の学習に切り替え
+                train_doc2vec = copy.copy(
+                    doc2vec_dict['2007'])
+
+            train_batch, return_flag = random_batch(
+                train_doc2vec, 32, train_start_idx)
+
+            if return_flag == False:
+                train_start_idx += 1
+            elif return_flag == True:
+                train_start_idx = 0
+
+            # 学習開始
+            model.train_step.run(feed_dict={mail_doc2vec: np.array(
+                train_batch[0]), mail_class: np.array(train_batch[1])})
+
+            # disp_freq(=20)回学習するごとに、テスト用メールに対する識別率を求める
+            if iteration % disp_freq == 0:
+                # 2005年のテスト用データに対する識別率を算出
+                test_batch_2005 = make_test_batch(doc2vec_2005)
+
+                feed_dict_2005 = {mail_doc2vec: np.array(
+                    test_batch_2005[0]), mail_class: np.array(test_batch_2005[1])}
+
+                test_accs[0].append(
+                    model.accuracy.eval(feed_dict=feed_dict_2005))
+
+                # 2006年のテスト用データに対する識別率を算出
+                test_batch_2006 = make_test_batch(doc2vec_2006)
+
+                feed_dict_2006 = {mail_doc2vec: np.array(
+                    test_batch_2006[0]), mail_class: np.array(test_batch_2006[1])}
+
+                test_accs[1].append(
+                    model.accuracy.eval(feed_dict=feed_dict_2006))
+
+                if iteration < 600:  # iterationが600未満の場合、2007年の分はNoneで埋める
+                    test_accs[2].append(None)
+                else:
+                    test_batch_2007 = make_test_batch(doc2vec_2007)
+
+                    feed_dict_2007 = {mail_doc2vec: np.array(
+                        test_batch_2007[0]), mail_class: np.array(test_batch_2007[1])}
+
+                    test_accs[2].append(
+                        model.accuracy.eval(feed_dict=feed_dict_2007))
+
+    x_iter = list(range(len(test_accs[0])))
+    plt.plot(x_iter, test_accs[0], marker='.',
+             label='2005')  # 2005年分の識別率をプロット
+    plt.plot(x_iter, test_accs[1], marker='.',
+             label='2006')  # 2006年分の識別率をプロット
+    plt.plot(x_iter, test_accs[2], marker='.',
+             label='2007')  # 2007年分の識別率をプロット
+    plt.show()
+
+
+def main():
+    """
+    read_doc2vec.pyにより実行される関数
+    """
+    common_path = '/Users/kawahara/Documents/01-programming/00-大学研究/02-result/trec_doc2vec/trec_year_doc2vec_model/'
+    model_info_orderdict = OrderedDict({
+        '2005': common_path + '2005_2000_mails_doc2vec.csv',
+        '2006': common_path + '2006_2000_mails_doc2vec.csv',
+        '2007': common_path + '2007_2000_mails_doc2vec.csv'
+    })
+
+    model_doc2vec_dict = dict()
+    model_doc2vec_learned_list = []
+    doc2vec_labels_list = []
+
+    PARAMETERS = parameter()
+
+    for doc2vec_label, file_path in model_info_orderdict.items():
+        with open(file_path, "r") as f:
+            reader = csv.reader(f)
+            model_doc2vec_dict[doc2vec_label] = [
+                [float(v) for v in row] for row in reader]
+
+    sess = tf.compat.v1.InteractiveSession()
+
+    mail_doc2vec = tf.compat.v1.placeholder(
+        tf.float32, shape=[None, 300])  # doc2vecの場合300
+    mail_class = tf.compat.v1.placeholder(
+        tf.float32, shape=[None, 2])  # 教師ラベルの種類　ham:[1,0],spam:[0,1]
+
+    # ニューラルネットワークモデルの構築(モデルの具体的な構成はmodel_for_doc2vec.pyを参照)
+    model = Model(mail_doc2vec, mail_class)
+
+    sess.run(tf.compat.v1.global_variables_initializer())
+
+    """
+    for doc2vec_label in model_info_orderdict.keys():
+        doc2vec_labels_list.append(doc2vec_label)
+        model_doc2vec_learned_list.append(model_doc2vec_dict[doc2vec_label])
+
+        train_task(model, PARAMETERS.ITERATIONS, PARAMETERS.DISP_FREQ,
+                   model_doc2vec_dict[doc2vec_label], doc2vec_labels_list, model_doc2vec_learned_list, mail_doc2vec, mail_class, sess, PARAMETERS.lams)
+
+        if doc2vec_label == '2007':
+            break
+
+        # 各ラベルのフィッシャー情報量の計算
+        model.compute_fisher(model_doc2vec_dict[doc2vec_label], sess,
+                             num_samples=200, plot_diffs=True)
+
+        # 重みとバイアスのパラメータ保存
+        model.stor()
+
+        if len(PARAMETERS.lams) == 1:
+            PARAMETERS.set_lams(50)
+    """
+
+    mvg(model, mail_doc2vec, mail_class, model_doc2vec_dict,
+        PARAMETERS.ITERATIONS, PARAMETERS.DISP_FREQ, sess, PARAMETERS.lams)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 01-program/03-SpamFilter_by_NeuralNetwork_using_EWC/EWC(main)/model_for_doc2vec_ver2.py
@@ -0,0 +1,183 @@
+"""
+2020/11/24日の輪行用のプログラム(詳しいことはOneDriveのPPTを参照)
+"""
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from copy import deepcopy
+from IPython import display
+
+tf.compat.v1.set_random_seed(2020)
+
+
+def weight_variable(shape):
+    """
+    重みの初期化
+    """
+    # shape分の各要素が正規分布かつ標準偏差の2倍までのランダムな値で初期化
+    initial = tf.random.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    """
+    バイアスの初期化
+    """
+    initial = tf.constant(0.1, shape=shape)  # shape分の各要素が0.1の行列
+    return tf.Variable(initial)
+
+
+class Model:
+    def __init__(self, x, y_):
+        """
+        ニューラルネットワークモデルの構築
+        入力層のユニット数：300
+        中間層のユニット数：100
+        出力層のユニット数：2
+        中間層の層数：1
+        使用する活性化関数：ReLU関数
+        使用する損失関数：交差エントロピー損失関数
+        """
+
+        in_dim = 300  # doc2vecを用いて得られたベクトルの次元数
+        out_dim = 2  # スパムメールと正規メールの2クラス
+
+        self.x = x  # input placeholder (x:[None,300])
+
+        # 入力層と中間層の間のパラメータ
+        W1 = weight_variable([in_dim, 128])
+        b1 = bias_variable([128])
+
+        # 中間層間のパラメータ
+        W2 = weight_variable([128, 32])
+        b2 = bias_variable([32])
+
+        # 中間層と出力層の間のパラメータ
+        W3 = weight_variable([32, out_dim])
+        b3 = bias_variable([out_dim])
+
+        # ReLU関数：活性化関数の一つで、入力した値が0以下のときに0、0より大きい場合はそのままの値を出力する関数
+        h1 = tf.nn.relu(tf.matmul(x, W1) + b1)  # hidden 1 layer
+        h2 = tf.nn.relu(tf.matmul(h1, W2) + b2)  # hidden 2 layer
+        self.y = tf.matmul(h2, W3) + b3  # output layer
+
+        self.var_list = [W1, b1, W2, b2, W3, b3]
+
+        # 交差エントロピー損失関数
+        self.cross_entropy = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=self.y))
+        self.set_vanilla_loss()
+
+        correct_prediction = tf.equal(tf.argmax(self.y, 1), tf.argmax(y_, 1))
+
+        self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    def compute_fisher(self, doc2vec_model, sess, num_samples=200, plot_diffs=False, disp_freq=10):
+        """
+        フィッシャー情報量の計算
+        """
+
+        self.F_accum = []
+        for v in range(len(self.var_list)):
+            self.F_accum.append(
+                np.zeros(self.var_list[v].get_shape().as_list()))
+
+        # 最も確率の高いクラスを選択
+        probs = tf.nn.softmax(self.y)
+        class_ind = tf.to_int32(tf.argmax(tf.log(probs), 1)[0])
+
+        if(plot_diffs):
+            # track differences in mean Fisher info
+            F_prev = deepcopy(self.F_accum)
+            mean_diffs = np.zeros(0)
+
+        sum_validation = []
+        sum_validation.extend(list(range(1000, 1100)))  # [1000,1001,...,1099]
+        sum_validation.extend(list(range(100)))  # [0,1,2,...,99]
+
+        for i in range(num_samples):  # num_samples=200
+            # フィッシャー情報行列の計算
+            im_ind = sum_validation.pop(0)
+
+            # 一次微分を計算する
+            # tf.gradientsを実行(sess.run)するうえで、スペースホルダーに該当する部分に、feed_dictで指定した値を使用する。
+            ders = sess.run(tf.gradients(tf.log(probs[0, class_ind]), self.var_list), feed_dict={
+                            self.x: np.array(doc2vec_model[im_ind]).reshape(1, 300)})
+
+            for v in range(len(self.F_accum)):
+                # 求めた勾配の二乗を該当するフィッシャー情報行列に加算していく。
+                self.F_accum[v] += np.square(ders[v])
+
+            if (plot_diffs):  # フィッシャー情報量のプロット処理
+                if i % disp_freq == 0 and i > 0:  # disp_freq=10
+                    # recording mean diffs of F
+                    F_diff = 0
+
+                    for v in range(len(self.F_accum)):
+                        # np.absolute:絶対値を求める。
+                        F_diff += np.sum(np.absolute(
+                            self.F_accum[v] / (i + 1) - F_prev[v]))
+
+                    mean_diff = np.mean(F_diff)
+                    mean_diffs = np.append(mean_diffs, mean_diff)
+
+                    for v in range(len(self.F_accum)):
+                        F_prev[v] = self.F_accum[v] / (i + 1)
+
+                    plt.plot(range(disp_freq + 1, i + 2, disp_freq),
+                             mean_diffs, marker='.')
+                    plt.xlabel("Number of samples")
+                    plt.ylabel("Mean absolute Fisher difference")
+                    display.display(plt.gcf())
+                    display.clear_output(wait=True)
+
+        for v in range(len(self.F_accum)):
+            self.F_accum[v] /= num_samples  # 平均を求める
+
+        plt.show()
+
+    def stor(self):
+        """
+        パラメータ(重みとバイアス)の保存
+        """
+        self.star_vars = []
+
+        for v in range(len(self.var_list)):
+            self.star_vars.append(self.var_list[v].eval())
+
+    def restore(self, sess):
+        """
+        保存していたパラメータの読み込み
+        """
+        if hasattr(self, "star_vars"):
+            for v in range(len(self.var_list)):
+                sess.run(self.var_list[v].assign(self.star_vars[v]))
+
+    def set_vanilla_loss(self):
+        """
+        Stochastic Gradient Descent(SGD)を用いてパラメータを更新
+        """
+        self.train_step = tf.compat.v1.train.GradientDescentOptimizer(
+            0.02).minimize(self.cross_entropy)
+
+    def update_ewc_loss(self, lam):
+        # elastic weight consolidation
+        # lam is weighting for previous language_task constraints
+        """
+        Elastic Weight Consolidationを用いてパラメータを更新
+        lam : 一つ前に学習したデータに対する重みづけ(過去のデータをどれだけ考慮するかを決めるパラメータ)
+        """
+
+        if not hasattr(self, "ewc_loss"):
+            self.ewc_loss = self.cross_entropy
+
+        # フィッシャー情報量の計算
+        for v in range(len(self.var_list)):
+            fisher = (lam / 2) * tf.reduce_sum(tf.multiply(self.F_accum[v].astype(
+                np.float32), tf.square(self.var_list[v] - self.star_vars[v])))
+
+            self.ewc_loss += fisher
+
+        # フィッシャー情報量を用いてパラメータ更新
+        self.train_step = tf.train.GradientDescentOptimizer(
+            0.02).minimize(self.ewc_loss)  # 学習率0.1として、ewcで求めた損失関数値を最小にするようにパラメータを学習する。
